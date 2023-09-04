3ceda72d00aaba272ee53e15649716557c1a280e
Arinc Sener <senerar-nc@windowslive.com>
2023-09-04 03:14:47 +0300

    screen is getting changed when button is pressed

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
 class StartScreen extends StatelessWidget {
-  const StartScreen({super.key});
+  const StartScreen(this.startQuiz, {super.key});
+
+  final void Function() startQuiz;
 
   @override
   Widget build(context) {
@@ -29,7 +31,7 @@ class StartScreen extends StatelessWidget {
             height: 80,
           ),
           OutlinedButton.icon( //our button
-            onPressed: () {},
+            onPressed: startQuiz,
             style: OutlinedButton.styleFrom(
               foregroundColor: Colors.white,
             ),
