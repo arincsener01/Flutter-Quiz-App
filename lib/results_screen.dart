7f12cafd85d8c855d3dc63ab4ec608341ff3c47a
Arinc Sener <senerar-nc@windowslive.com>
2023-09-26 01:41:39 +0300

    changes made in the result screen

--- lib/results_screen.dart
+++ lib/results_screen.dart
@@ -1,21 +1,28 @@
 import 'package:flutter/material.dart';
+
 import 'package:flutter_quiz_app/data/questions.dart';
+import 'package:flutter_quiz_app/questions_summary.dart';
 
 class ResultsScreen extends StatelessWidget {
-  const ResultsScreen({super.key, required this.chosenAnswers});
+  const ResultsScreen({
+    super.key,
+    required this.chosenAnswers,
+  });
 
   final List<String> chosenAnswers;
 
   List<Map<String, Object>> getSummaryData() { //object is a flexible type that allows all kinds of values. This map can be used as a data structure that groups different kinds key value pairs together.
     final List<Map<String, Object>> summary = [];
 
-    for(var i = 0; i < chosenAnswers.length; i++) {
-      summary.add({
-        'question_index': i,
-        'question': questions[i].text,
-        'correct_answer': questions[i].answers[0],
-        'user_answer': chosenAnswers[i]
-      });
+    for (var i = 0; i < chosenAnswers.length; i++) {
+      summary.add(
+        {
+          'question_index': i,
+          'question': questions[i].text,
+          'correct_answer': questions[i].answers[0],
+          'user_answer': chosenAnswers[i]
+        },
+      );
     }
 
     return summary;
@@ -23,6 +30,12 @@ class ResultsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final summaryData = getSummaryData();
+    final numTotalQuestions = questions.length;
+    final numCorrectQuestions = summaryData.where((data) {
+      return data['user_answer'] == data['correct_answer'];
+    }).length;
+
     return SizedBox(
       width: double.infinity,
       child: Container(
@@ -30,11 +43,13 @@ class ResultsScreen extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Text('You answered X out of Y questions correctly!'),
+            Text(
+              'You answered $numCorrectQuestions out of $numTotalQuestions questions correctly!',
+            ),
             const SizedBox(
               height: 30,
             ),
-            const Text('List of answers and questions...'),
+            QuestionsSummary(summaryData),
             const SizedBox(
               height: 30,
             ),
